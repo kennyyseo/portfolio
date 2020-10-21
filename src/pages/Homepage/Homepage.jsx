9f7279d7ee9ea69a55e8eb9974b581f4137cbc83
Kenny Seo <kennyyseo@gmail.com>
2020-10-21 13:37:19 -0700

    Update homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -12,32 +12,42 @@ const Homepage = () => {
             </div>
             <div className='namecard'>
                 <img className='photo' src="https://i.imgur.com/tp3CDA5.jpgs" alt="" />
-                <p className='description'> As a recent graduate of the Software Engineering Immersive program at General Assembly, I've discovered the exciting world of programming! My time as a Sales Executive in the programmatic media space has made me aware of the lack of streamlined communication between customers' needs and product/engineering teams' roadmaps, and I look forward to helping bridge the gap on your team!</p>
+                <p className='description'> As a recent graduate of the Software Engineering Immersive course at General Assembly, I've discovered the exciting world of programming! My time as a Sales Executive in the programmatic media space has made me aware of the lack of streamlined communication between customers' needs and product/engineering teams' roadmaps, and I look forward to helping bridge the gap on your team!</p>
             </div>
             <div className='skills'>
                 <h2>Skills</h2>
             </div>
             <div className='skillscard'>
-                <p>MongoDB</p>
-                <p>Express</p>
-                <p>React</p>
-                <p>Node.js</p>
-                <p>Javascript</p>
-                <p>HTML</p>
-                <p>CSS</p>
-                <p>Python</p>
-                <p>Heroku</p>
-                <p>Github</p>
-                <p>Google Oauth</p>
-                <p>Passport</p>
-                <p>Jquery</p>
-                <p>PostgreSQL</p>
+                <div className='col1'>
+                    <p className='skills-item'>HTML</p>
+                    <p className='skills-item'>CSS</p>
+                    <p className='skills-item'>Javascript</p>
+                    <p className='skills-item'>Python</p>
+                    <p className='skills-item'>Github</p>
+                </div>
+                <div className='col2'>
+                    <p className='skills-item'>MongoDB</p>
+                    <p className='skills-item'>Express</p>
+                    <p className='skills-item'>React</p>
+                    <p className='skills-item'>Node.js</p>
+                    <p className='skills-item'>Mongoose</p>
+                </div>
+                <div className='col3'>
+                    <p className='skills-item'>Google Oauth</p>
+                    <p className='skills-item'>Passport</p>
+                    <p className='skills-item'>Jquery</p>
+                    <p className='skills-item'>PostgreSQL</p>
+                    <p className='skills-item'>Heroku</p>
+                </div>
+
+
             </div>
             <div className='education'>
                 <h2>Education</h2>
             </div>
             <div className='educationcard'>
-                <p className='educationlist'> As a recent graduate of the Software Engineering Immersive program at General Assembly, I've discovered the exciting world of programming! My time as a Sales Executive in the programmatic media space has made me aware of the lack of streamlined communication between customers' needs and product/engineering teams' roadmaps, and I look forward to helping bridge the gap on your team!</p>
+                <p>General Assembly: Software Engineering Immersive - 2020 </p>
+                <p>University of Southern California: B.S. in Business Management - 2013</p>
             </div>
         </div>
     )
